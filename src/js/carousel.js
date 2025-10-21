d209f5ba05a773727317964432d6c4981b9fd389
Juan Almeida <juanalgal2@gmail.com>
2025-10-20 23:51:00 -0300

    adding things

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -6,4 +6,4 @@ const emblaApi = EmblaCarousel(emblaNode, options, plugins)
 const emblaNode2 = document.querySelector('.embla2') 
 const options2 = { loop: true, direction: 'rtl' }  
 const plugins2 = [EmblaCarouselAutoplay({delay: 1000})]
-const emblaApi2 = EmblaCarousel(emblaNode2, options2, plugins2)
+const emblaApi2 = EmblaCarousel(emblaNode2, options2, plugins2) 
